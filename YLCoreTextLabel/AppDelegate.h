622763fa3315e9e3f918c50c05e6d924b2febb26
qiuyuliang <iqiuyuliang@163.com>
2015-03-04 11:21:45 +0800

    Initial Commit

---
+++ YLCoreTextLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  YLCoreTextLabel
+//
+//  Created by 邱 育良 on 15/3/4.
+//  Copyright (c) 2015年 Qiu Yuliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
